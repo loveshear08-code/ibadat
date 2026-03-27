5f5905cb95f8d15e7292efe118ba6649f11b5166
ABDUL SHIS MAHAMMAD MOLLICK <loveshear08@gmail.com>
2026-03-27 21:27:15 +0530

    Update home.js

--- js/home.js
+++ js/home.js
@@ -121,8 +121,15 @@ let div=document.createElement("div");
 div.className="prayer-box";
 div.innerHTML=`${p[0]}<br>${toBN(p[1])}`;
 
-div.style.cursor="pointer";
-div.onclick=()=>openPage("azan-setting");
+/* ❗ Sunrise disable */
+if(p[0] !== "সূর্যোদয়"){
+    div.style.cursor="pointer";
+    div.onclick=()=>openPage("azan-setting");
+}else{
+    div.style.opacity="0.6";   // optional UI effect
+    div.style.cursor="default";
+      }
+  
 
 grid.appendChild(div);
 });
